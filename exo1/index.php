547a9057d4e87455c3c7d877726bdfd6f55dc7de
NathSourire <nathsourire@hotmail.com>
2023-08-21 16:23:51 +0200

    4

--- exo1/index.php
+++ exo1/index.php
@@ -19,11 +19,21 @@ $name = 'Nathalie';
         <div class="row">
             <img class="imgLogo col-3" src="./public/assets/img/PHP-logo.png" alt="Logo PHP">
             <h1 class="col-7">Exercice 1</h1>
-            <p>Créer une variable name et l'initialiser avec la valeur de votre choix. Afficher son contenu.</p>
+            <p>Faire une page index.php.
+                Dans cette page, écrire un lien qui renvoie vers index.php?lastname=Nemare&firstname=Jean
+                Tester sur cette page que tous les paramètres de cette URL existent et dans ce cas, les afficher.</p>
         </div>
     </header>
-    <main class="">
+    <main class="container- fluid">
+        <div class="row">
+            <div class="results position-absolute top-50 start-50 translate-middle ">
+                <p>
+                    <?php
 
+                    ?>
+                </p>
+            </div>
+        </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
--- exo3/index.php
+++ exo3/index.php
@@ -11,19 +11,28 @@ $name = 'Nathalie';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="./public/assets/css/style.css">
-    <title>PHP exo 1</title>
+    <title>PHP exo 3</title>
 </head>
 
 <body>
     <header class="container-fluid">
         <div class="row">
             <img class="imgLogo col-3" src="./public/assets/img/PHP-logo.png" alt="Logo PHP">
-            <h1 class="col-7">Exercice 1</h1>
-            <p>Créer une variable name et l'initialiser avec la valeur de votre choix. Afficher son contenu.</p>
+            <h1 class="col-7">Exercice 3</h1>
+            <p>Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL existent  et les afficher : 
+                index.php?startDate=2/05/2016&endDate=27/11/2016</p>
         </div>
     </header>
-    <main class="">
+    <main class="container- fluid">
+        <div class="row">
+            <div class="results position-absolute top-50 start-50 translate-middle ">
+                <p>
+                    <?php
 
+                    ?>
+                </p>
+            </div>
+        </div>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
